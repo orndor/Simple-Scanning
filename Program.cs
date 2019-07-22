4c42a74f96d8985f9cbac3a29fad243dcdd9137d
Rob Orndoff <rorndoff@gmail.com>
2019-07-22 18:23:02 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Scanning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
