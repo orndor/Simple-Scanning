827985f60634e49d88c8b6d1f7faa14a5423972f
Rob Orndoff <rorndoff@gmail.com>
2019-07-22 23:15:05 -0400

    30% Completed on intial build.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Simple_Scanning
 {
@@ -6,7 +7,84 @@ namespace Simple_Scanning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char menuInput = ' ';
+            char[] delimiterChars = { '¿' };
+            string fileName = @"Task-List.txt";
+            int counter = 0;
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+            string[] lines = File.ReadAllLines(fileName);
+            for (int i = counter; i < 20; i++)
+            {
+                string line = lines[i];
+                string[] statusSeparator = line.Split(delimiterChars);
+                Console.WriteLine($"{statusSeparator[1]}");
+            }
+            counter += 20;
+            Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+            menuInput = Console.ReadKey().KeyChar;
+            do
+            {
+                if (menuInput == 'N' || menuInput == 'n')
+                {
+                    for (int i = counter; i <= counter + 19; i++)
+                    {
+                        string line = lines[i];
+                        string[] statusSeparator = line.Split(delimiterChars);
+                        Console.WriteLine($"{statusSeparator[1]}");
+                    }
+                    counter += 20;
+                    Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+                    menuInput = Console.ReadKey().KeyChar;
+                }
+                else if (menuInput == 'p' || menuInput == 'p')
+                {
+                    for (int i = counter - 40; i <= counter - 21; i++)
+                    {
+                        string line = lines[i];
+                        string[] statusSeparator = line.Split(delimiterChars);
+                        Console.WriteLine($"{statusSeparator[1]}");
+                    }
+                    counter -= 20;
+                    Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+                    menuInput = Console.ReadKey().KeyChar;
+                }
+                else if (menuInput == 'I' || menuInput == 'i')
+                {
+                    Console.WriteLine("Enter a new task on a single line.  Hit enter when complete.");
+                    string newTask = Console.ReadLine();
+                    using (StreamWriter stream = File.AppendText(@"Task-List.txt"))
+                    {
+                        stream.WriteLine($"O¿{lines.Length + 1}.{newTask}");
+                    }
+                    for (int i = 0; i <= 19; i++)
+                    {
+                        string line = lines[i];
+                        string[] statusSeparator = line.Split(delimiterChars);
+                        Console.WriteLine($"{statusSeparator[1]}");
+                    }
+                    Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+                    menuInput = Console.ReadKey().KeyChar;
+                }
+                else if (menuInput == 'X' || menuInput == 'x')
+                {
+                    System.Environment.Exit(0);
+                }
+                else if (menuInput == 'E' || menuInput == 'e')
+                {
+                    Console.WriteLine("Implment task for editing an item");
+                    Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+                    menuInput = Console.ReadKey().KeyChar;
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease enter a valid choice.");
+                    Console.WriteLine("Options: N for next 20 tasks.  P for previous 20 tasks.  I to insert a new task.  X to Exit.  E to edit an item.");
+                    menuInput = Console.ReadKey().KeyChar;
+                }
+            } while (menuInput != 'N' || menuInput == 'n' || menuInput == 'P' || menuInput == 'p' || menuInput == 'I' || menuInput == 'i' || menuInput == 'X' || menuInput == 'x' || menuInput == 'E' || menuInput == 'e');
         }
     }
 }
---
+++ docs/Requirements.md
@@ -0,0 +1,42 @@
+# Requirements
+
+## Background and User Requirements
+
+* [Mark Forster - Square Space blog](http://markforster.squarespace.com/blog/2017/12/2/simple-scanning-the-rules.html) as feeder information on this project.
+  * Long lists for task management.
+  * Develop software for Simple Scanning.
+    * Actions which can be completed on a list of things:
+      * Cross-Out (Completed)
+      * Re-Enter (Moved to the bottom of the list)
+      * Skip (Do later)
+    * Console UI.
+    * Single line items, user enters lines of text (freeform)
+    * view by page:
+    * 1st page beings with first unactioned item. (everything about this has been completed and deleted.)
+    * pages containing no unactioned items are persevered and display
+    * added items always become the last item on the "long list"
+    * Pages of items should contain 20 items
+    * Completed items should be notated by grey text.
+    * Current item of work should be highlighted (invert foreground and background color)
+    * Data stored in a flat text file, in the current directory
+    * should be able to add a task at any time
+    * Stretch item
+    "item info" e.g. entry data or # of re-entries which displays separately from task list.
+
+## Functional Requirements
+
+* Upon execution of the of program at the command line, and the user has not exited the program, the program shall run in a console UI until the user presses a key combination which causes the program to exit.
+
+* The user shall see single line items of up to 20 tasks which have been input by the user in free text format, while the program is running.
+
+* The user shall be able to see the tasks on the task list over 20 items in length by pressing a key on the keyboard which then displays up the next 20 items on the task list.
+
+* The user shall be able to mark individual items on the task list as completed which will then change the item's text color to grey.
+
+* Tasks input by the user will always go to the end of task list.
+
+* The task list shall be stored in a text file within the same directory from where the application executable is stored.
+
+* The user shall be able to select an item as the current item of work, and when selected the item should be highlighted (invert foreground and background colors)
+
+* The user shall be able to select an item to skip, which moves it to the top of the task list.
