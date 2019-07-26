d72f93db8ebb1c2d093eb742076d9fff69cac6a6
Rob Orndoff <rorndoff@gmail.com>
2019-07-26 15:34:12 -0400

    More bug fixes.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 // Project Name: Simply_Scanning
 // Author: Orndoff, Robert K.
 // Date created: 07/22/2019
-// Date last modified: 07/25/2019
+// Date last modified: 07/26/2019
 //
 // Purpose: MSSA Homework assignment to create a simple task tracking application based on 
-// Mark Forster Simply Scanning technique: http://markforster.squarespace.com/blog/2017/12/2/simple-scanning-the-rules.html
+// Mark Forster Simple Scanning technique: http://markforster.squarespace.com/blog/2017/12/2/simple-scanning-the-rules.html
 //
 // C#
 using System;
@@ -50,91 +50,101 @@ namespace Simple_Scanning
                 }
                 else if (menuInput == 'E' || menuInput == 'e')
                 {
-                    MenuPrinter("Enter the task number you wish to edit, and then press Enter:");
-                    int taskNumber = int.Parse(Console.ReadLine());
-
-                    if (taskNumber > taskLists.Count)
+                    try
                     {
-                        MenuPrinter("That task number dosen't exist.  Please try again.");
-                    }
-                    else
-                    {
-                        MenuPrinter("Would you like to [D]ismiss, [R]eenter, or [S]tart working the selected task? Or you can go [B]ack.");
-                        char editOption = Console.ReadKey().KeyChar;
+                        MenuPrinter("Enter the task number you wish to edit, and then press Enter:");
+                        int taskNumber = int.Parse(Console.ReadLine());
 
-                        if (editOption == 'D' || editOption == 'd')
+                        if (taskNumber > taskLists.Count)
                         {
-                            string line = taskLists[taskNumber - 1];
-                            string[] statusSeparator = line.Split(delimiterChars);
-                            taskLists[taskNumber - 1] = $"D{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
-                            FileWriter(delimiterChars, taskLists, fileName);
-                            ConsoleWriter(delimiterChars, 0, taskLists);
-                        }
-                        else if (editOption == 'R' || editOption == 'r')
-                        {
-                            string line = taskLists[taskNumber - 1];
-                            string[] statusSeparator = line.Split(delimiterChars);
-
-                            using (StreamWriter streamLine = File.AppendText(@"Task-List.txt"))
-                            {
-                                statusSeparator = line.Split(delimiterChars);
-                                streamLine.WriteLine($"O{delimiterChars}{taskLists.Count + 1}.{delimiterChars}{statusSeparator[2]}");
-                            }
-
-                            taskLists = FileReader(fileName);
-                            taskLists[taskNumber - 1] = $"D{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
-                            FileWriter(delimiterChars, taskLists, fileName);
-                            currentPlaceOnList = ConsoleWriter(delimiterChars, 0, taskLists);
-                        }
-                        else if (editOption == 'S' || editOption == 's')
-                        {
-                            bool taskAlreadyStarted = false;
-                            string line = taskLists[0];
-                            string[] statusSeparator = line.Split(delimiterChars);
-
-                            for (int i = 0; i < taskLists.Count; i++)
-                            {
-                                line = taskLists[i];
-                                statusSeparator = line.Split(delimiterChars);
-
-                                if (statusSeparator[0] == "S")
-                                {
-                                    taskAlreadyStarted = true;
-                                }
-                            }
-                            if (taskAlreadyStarted)
-                            {
-                                MenuPrinter("You're already working on a task.  You'll need to dismiss or reenter the task to start a new one.");
-                            }
-                            else
-                            {
-                                line = taskLists[taskNumber - 1];
-                                statusSeparator = line.Split(delimiterChars);
-                                taskLists[taskNumber - 1] = $"S{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
-                                FileWriter(delimiterChars, taskLists, fileName);
-                                ConsoleWriter(delimiterChars, 0, taskLists);
-                            }
-                        }
-                        else if (editOption == 'B' || editOption == 'b')
-                        {
-                            continue;
+                            MenuPrinter("That task number dosen't exist.  Please try again.");
                         }
                         else
                         {
-                            MenuPrinter("Please enter a valid choice.");
+                            MenuPrinter("Would you like to [D]ismiss, [R]eenter, or [S]tart working the selected task? Or you can go [B]ack.");
+                            char editOption = Console.ReadKey().KeyChar;
+
+                            if (editOption == 'D' || editOption == 'd')
+                            {
+                                string line = taskLists[taskNumber - 1];
+                                string[] statusSeparator = line.Split(delimiterChars);
+                                taskLists[taskNumber - 1] = $"D{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
+                                FileWriter(delimiterChars, taskLists, fileName);
+                                ConsoleWriter(delimiterChars, 0, taskLists);
+                            }
+                            else if (editOption == 'R' || editOption == 'r')
+                            {
+                                string line = taskLists[taskNumber - 1];
+                                string[] statusSeparator = line.Split(delimiterChars);
+
+                                using (StreamWriter streamLine = File.AppendText(@"Task-List.txt"))
+                                {
+                                    statusSeparator = line.Split(delimiterChars);
+                                    streamLine.WriteLine($"O{delimiterChars}{taskLists.Count + 1}.{delimiterChars}{statusSeparator[2]}");
+                                }
+
+                                taskLists = FileReader(fileName);
+                                taskLists[taskNumber - 1] = $"D{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
+                                FileWriter(delimiterChars, taskLists, fileName);
+                                currentPlaceOnList = ConsoleWriter(delimiterChars, 0, taskLists);
+                            }
+                            else if (editOption == 'S' || editOption == 's')
+                            {
+                                bool taskAlreadyStarted = false;
+                                string line = taskLists[0];
+                                string[] statusSeparator = line.Split(delimiterChars);
+
+                                for (int i = 0; i < taskLists.Count; i++)
+                                {
+                                    line = taskLists[i];
+                                    statusSeparator = line.Split(delimiterChars);
+
+                                    if (statusSeparator[0] == "S")
+                                    {
+                                        taskAlreadyStarted = true;
+                                    }
+                                }
+                                if (taskAlreadyStarted)
+                                {
+                                    MenuPrinter("You're already working on a task.  You'll need to dismiss or reenter the task to start a new one.");
+                                }
+                                else
+                                {
+                                    line = taskLists[taskNumber - 1];
+                                    statusSeparator = line.Split(delimiterChars);
+                                    taskLists[taskNumber - 1] = $"S{delimiterChars}{statusSeparator[1]}{delimiterChars}{statusSeparator[2]}";
+                                    FileWriter(delimiterChars, taskLists, fileName);
+                                    ConsoleWriter(delimiterChars, 0, taskLists);
+                                }
+                            }
+                            else if (editOption == 'B' || editOption == 'b')
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                MenuPrinter("Not a valid choice.");
+                            }
                         }
+                        
                     }
+                    catch
+                    {
+                        MenuPrinter("Not a valid choice.");
+                    }
+
                 }
                 else if (menuInput == 'X' || menuInput == 'x')
                 {
-                    System.Environment.Exit(0);
+                    MenuPrinter("\n\nGoodbye!\n");
+                    Environment.Exit(0);
                 }
                 else
                 {
                     MenuPrinter("Please enter a valid choice."); ;
                 }
                 menuInput = ' ';
-            } while (menuInput == 'N' || menuInput == 'n' || menuInput == 'I' || menuInput == 'i' || menuInput == 'X' || menuInput == 'x' || menuInput == 'E' || menuInput == 'e' || menuInput == ' ');
+            } while (menuInput == ' ');
         }
 
 
@@ -153,10 +163,9 @@ namespace Simple_Scanning
                 File.Create(fileName);
             }
 
-            var tasksFile = File.ReadAllLines(fileName);
-            var taskLists = new List<string>(tasksFile);
+            var taskList = new List<string>(File.ReadAllLines(fileName));
 
-            return taskLists;
+            return taskList;
         }
 
 
@@ -198,8 +207,7 @@ namespace Simple_Scanning
                 }
             }
             catch
-            {
-            }
+            { ; }
 
             return taskLists;
         }
